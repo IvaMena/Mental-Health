2ab480c9025530e3ad07ac27538af4d31882b674
D3Lphin3 <41123990+D3Lphin3@users.noreply.github.com>
2018-08-01 15:06:12 -0700

    Add files via upload

---
+++ personalityjs.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
